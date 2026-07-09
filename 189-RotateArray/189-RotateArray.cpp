09b868153ca2910d19ab2224426e1ff21495b129
Vishwa6566 <24243123@drngpit.ac.in>
2026-07-09 10:40:39 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 10:40:39 AM
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
