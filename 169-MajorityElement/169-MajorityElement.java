159803a371b259c53a7f73ef2a416ab858a0ce5f
Vishwa6566 <24243123@drngpit.ac.in>
2026-07-09 10:41:13 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 10:41:13 AM
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
