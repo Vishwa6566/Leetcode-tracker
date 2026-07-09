4aff506b382f84aa28de518786741176d2cfdb6c
Vishwa6566 <24243123@drngpit.ac.in>
2026-07-09 10:41:30 +0530

    Create 52-NQueensIi

---
+++ 52-NQueensIi/52-NQueensIi.java
@@ -0,0 +1,42 @@
+// Last updated: 7/9/2026, 10:41:30 AM
+class Solution {
+    int count = 0;
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (char[] row : board)
+            Arrays.fill(row, '.');
+
+        solve(board, 0);
+        return count;
+    }
+
+    private void solve(char[][] board, int row) {
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                solve(board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q') return false;
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q') return false;
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++)
+            if (board[i][j] == 'Q') return false;
+
+        return true;
+    }
+}
