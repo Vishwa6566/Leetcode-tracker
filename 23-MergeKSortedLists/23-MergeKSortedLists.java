f52953e34ac04ddc261d0ec8e9c0fdb6e5d784a6
Vishwa6566 <24243123@drngpit.ac.in>
2026-07-09 10:42:12 +0530

    Create 23-MergeKSortedLists

---
+++ 23-MergeKSortedLists/23-MergeKSortedLists.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 10:42:12 AM
+import java.util.*;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode node : lists) {
+            if (node != null)
+                pq.offer(node);
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            curr.next = node;
+            curr = curr.next;
+
+            if (node.next != null)
+                pq.offer(node.next);
+        }
+
+        return dummy.next;
+    }
+}
