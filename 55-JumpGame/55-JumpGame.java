2ef1ec36026aef274278c33dcb8bfdea260070f4
Vishwa6566 <24243123@drngpit.ac.in>
2026-07-09 10:41:26 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 10:41:25 AM
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) return false;
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+
+        return true;
+    }
+}
