bbcc5bb7d4591f8f15e5c6e86908bbaf77159f70
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 14:01:07 +0530

    Create 143-ReorderList

---
+++ 143-ReorderList/143-ReorderList.java
@@ -0,0 +1,44 @@
+// Last updated: 8/5/2026, 2:01:07 PM
+class Solution {
+
+    public void reorderList(ListNode head) {
+
+        if (head == null || head.next == null)
+            return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        slow.next = null;
+
+        while (curr != null) {
+
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+
+            first.next = second;
+            second.next = t1;
+
+            first = t1;
+            second = t2;
+        }
+    }
+}
