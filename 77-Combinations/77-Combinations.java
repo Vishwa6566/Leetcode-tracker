77b8799e02616da6789b52a737d45e8ce946660f
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 14:15:02 +0530

    Create 77-Combinations

---
+++ 77-Combinations/77-Combinations.java
@@ -0,0 +1,24 @@
+// Last updated: 8/5/2026, 2:15:01 PM
+class Solution {
+
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtrack(1, n, k, new ArrayList<>());
+        return ans;
+    }
+
+    void backtrack(int start, int n, int k, List<Integer> list) {
+
+        if (list.size() == k) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            list.add(i);
+            backtrack(i + 1, n, k, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
