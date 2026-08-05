6b290fd3d3e8113f70f78e94410854583938eabd
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 14:02:49 +0530

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -0,0 +1,26 @@
+// Last updated: 8/5/2026, 2:02:48 PM
+class Solution {
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        int left = height(root.left);
+        if (left == -1)
+            return -1;
+
+        int right = height(root.right);
+        if (right == -1)
+            return -1;
+
+        if (Math.abs(left - right) > 1)
+            return -1;
+
+        return Math.max(left, right) + 1;
+    }
+}
