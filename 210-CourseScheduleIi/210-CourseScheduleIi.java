eebf4c3970f57e4439ff793d98111aacc55ae3e5
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 13:57:47 +0530

    Create 210-CourseScheduleIi

---
+++ 210-CourseScheduleIi/210-CourseScheduleIi.java
@@ -0,0 +1,45 @@
+// Last updated: 8/5/2026, 1:57:47 PM
+import java.util.*;
+
+class Solution {
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++)
+            graph.add(new ArrayList<>());
+
+        int[] indegree = new int[numCourses];
+
+        for (int[] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                queue.offer(i);
+
+        int[] order = new int[numCourses];
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+
+            int curr = queue.poll();
+            order[index++] = curr;
+
+            for (int next : graph.get(curr)) {
+
+                indegree[next]--;
+
+                if (indegree[next] == 0)
+                    queue.offer(next);
+            }
+        }
+
+        return index == numCourses ? order : new int[0];
+    }
+}
