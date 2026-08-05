f1eb2f130b5a85ce6cec8631f004790b504c004f
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 13:58:05 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -0,0 +1,23 @@
+// Last updated: 8/5/2026, 1:58:04 PM
+class Solution {
+
+    public boolean isIsomorphic(String s, String t) {
+
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapS[c1] != mapT[c2])
+                return false;
+
+            mapS[c1] = i + 1;
+            mapT[c2] = i + 1;
+        }
+
+        return true;
+    }
+}
