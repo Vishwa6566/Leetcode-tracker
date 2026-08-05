a23ba64f707b969b10ff8da721e96f2c36ca53a7
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 14:01:24 +0530

    Create 138-CopyListWithRandomPointer

---
+++ 138-CopyListWithRandomPointer/138-CopyListWithRandomPointer.java
@@ -0,0 +1,45 @@
+// Last updated: 8/5/2026, 2:01:23 PM
+class Solution {
+
+    public Node copyRandomList(Node head) {
+
+        if (head == null)
+            return null;
+
+        Node curr = head;
+
+        while (curr != null) {
+
+            Node copy = new Node(curr.val);
+
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        curr = head;
+
+        while (curr != null) {
+
+            if (curr.random != null)
+                curr.next.random = curr.random.next;
+
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node dummy = new Node(0);
+        Node copyCurr = dummy;
+
+        while (curr != null) {
+
+            copyCurr.next = curr.next;
+            curr.next = curr.next.next;
+
+            curr = curr.next;
+            copyCurr = copyCurr.next;
+        }
+
+        return dummy.next;
+    }
+}
