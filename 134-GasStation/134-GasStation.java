0dca7f9dd45a56b40eb184af22c0f6a37066bc4a
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 14:01:36 +0530

    Create 134-GasStation

---
+++ 134-GasStation/134-GasStation.java
@@ -0,0 +1,23 @@
+// Last updated: 8/5/2026, 2:01:35 PM
+class Solution {
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+
+            total += gas[i] - cost[i];
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return total >= 0 ? start : -1;
+    }
+}
