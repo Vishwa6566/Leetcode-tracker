e110255a0984a68b8f57009358791cf68a4a4156
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 14:02:17 +0530

    Create 120-Triangle

---
+++ 120-Triangle/120-Triangle.java
@@ -0,0 +1,20 @@
+// Last updated: 8/5/2026, 2:02:16 PM
+import java.util.*;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) +
+                        Math.min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+}
