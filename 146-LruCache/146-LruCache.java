cade8d2a67aea8238c3c745f91105a5b5969a314
Vishwa6566 <24243123@drngpit.ac.in>
2026-08-05 14:00:57 +0530

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.java
@@ -0,0 +1,31 @@
+// Last updated: 8/5/2026, 2:00:56 PM
+import java.util.*;
+
+class LRUCache {
+
+    private int capacity;
+    private LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+
+        this.capacity = capacity;
+
+        map = new LinkedHashMap<>(capacity, 0.75f, true);
+    }
+
+    public int get(int key) {
+
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+
+        map.put(key, value);
+
+        if (map.size() > capacity) {
+
+            int firstKey = map.keySet().iterator().next();
+            map.remove(firstKey);
+        }
+    }
+}
